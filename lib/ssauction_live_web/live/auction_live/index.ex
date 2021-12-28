5b674c093e85221bcd908eab8ef227a7b3684c8b
Daryl Spitzer <dspitzer@roku.com>
2021-12-28 10:03:37 -0700

    clicking on auction index table rows goes to auction pages (plus code cleanup)

--- lib/ssauction_live_web/live/auction_live/index.ex
+++ lib/ssauction_live_web/live/auction_live/index.ex
@@ -3,6 +3,7 @@ defmodule SSAuctionWeb.AuctionLive.Index do
 
   alias SSAuction.Auctions
   alias SSAuction.Auctions.Auction
+  alias SSAuctionWeb.Router.Helpers, as: Routes
 
   @impl true
   def mount(_params, _session, socket) do
@@ -10,33 +11,7 @@ defmodule SSAuctionWeb.AuctionLive.Index do
   end
 
   @impl true
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Auction")
-    |> assign(:auction, Auctions.get_auction!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Auction")
-    |> assign(:auction, %Auction{})
-  end
-
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Listing Auctions")
-    |> assign(:auction, nil)
-  end
-
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    auction = Auctions.get_auction!(id)
-    {:ok, _} = Auctions.delete_auction(auction)
-
-    {:noreply, assign(socket, :auctions, Auctions.list_auctions())}
+  def handle_event("auction", %{"id" => id}, socket) do
+    {:noreply, redirect(socket, to: Routes.auction_show_path(socket, :show, id))}
   end
 end
