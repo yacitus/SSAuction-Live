f04a99b72157578133bbfc26ff33877e385df31c
Daryl Spitzer <dspitzer@roku.com>
2021-12-28 16:39:48 -0700

    move duplicated code

--- lib/ssauction_live_web/live/auction_live/show.ex
+++ lib/ssauction_live_web/live/auction_live/show.ex
@@ -4,10 +4,6 @@ defmodule SSAuctionWeb.AuctionLive.Show do
   alias SSAuction.Auctions
   alias SSAuction.Cldr
 
-  @default_locale "en"
-  @default_timezone "UTC"
-  @default_timezone_offset 0
-
   @impl true
   def mount(_params, _session, socket) do
      socket =
@@ -17,21 +13,6 @@ defmodule SSAuctionWeb.AuctionLive.Show do
       |> assign_timezone_offset()
 
     {:ok, socket}
-  end
-
-  defp assign_locale(socket) do
-    locale = get_connect_params(socket)["locale"] || @default_locale
-    assign(socket, locale: locale)
-  end
-
-  defp assign_timezone(socket) do
-    timezone = get_connect_params(socket)["timezone"] || @default_timezone
-    assign(socket, timezone: timezone)
-  end
-
-  defp assign_timezone_offset(socket) do
-    timezone_offset = get_connect_params(socket)["timezone_offset"] || @default_timezone_offset
-    assign(socket, timezone_offset: timezone_offset)
   end
 
   @impl true
--- lib/ssauction_live_web/live/live_helpers.ex
+++ lib/ssauction_live_web/live/live_helpers.ex
@@ -57,4 +57,23 @@ defmodule SSAuctionWeb.LiveHelpers do
     |> JS.hide(to: "#modal", transition: "fade-out")
     |> JS.hide(to: "#modal-content", transition: "fade-out-scale")
   end
+
+  @default_locale "en"
+  @default_timezone "UTC"
+  @default_timezone_offset 0
+
+  def assign_locale(socket) do
+    locale = get_connect_params(socket)["locale"] || @default_locale
+    assign(socket, locale: locale)
+  end
+
+  def assign_timezone(socket) do
+    timezone = get_connect_params(socket)["timezone"] || @default_timezone
+    assign(socket, timezone: timezone)
+  end
+
+  def assign_timezone_offset(socket) do
+    timezone_offset = get_connect_params(socket)["timezone_offset"] || @default_timezone_offset
+    assign(socket, timezone_offset: timezone_offset)
+  end
 end
